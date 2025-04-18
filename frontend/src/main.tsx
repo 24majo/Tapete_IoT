76a5173d00bffdf739153f0e8c6b9bf24637ad24
24majo <157389928+24majo@users.noreply.github.com>
2025-04-18 02:07:15 -0600

    feat(main): configure and register navigation routes in main file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import '@mantine/core/styles.css';
 import './index.css'
 import App from './App.tsx'
 import Inicio_Alumno from './pages/sessions/session_alumn.tsx'
 import Inicio_Admin from './pages/sessions/session_admin.tsx'
+import PerfilAlumn from './pages/sessions/perfil_alumn.tsx';
+
 import Menu from './pages/menus/menu_selection.tsx'
 import Menu_Letters from './pages/menus/menu_letters.tsx'
+
 import Menu_Numbers from './pages/menus/menu_numbers.tsx'
-import { MantineProvider } from "@mantine/core";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import '@mantine/core/styles.css';
+import N_juego_1 from './pages/numbers/select_number.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>  
@@ -19,8 +23,13 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<App />} />
           <Route path='/Inicio_Alumno' element = {<Inicio_Alumno />}/>
           <Route path='/Inicio_Admin' element = {<Inicio_Admin />}/>
+
           <Route path='/MenuSeleccion' element = {<Menu /> } />
+          <Route path='/MyPerfil' element = {<PerfilAlumn /> }/>
+
           <Route path='/MenuNumeros' element = {<Menu_Numbers /> } />
+          <Route path='/JuegoNumeros1' element = {<N_juego_1 />} />
+
           <Route path='/MenuLetras' element = {<Menu_Letters /> } />
         </Routes>
       </BrowserRouter>
